7d482fb70ecda24b9e697e64d4490af50241c024
eric <Eric355402@yahoo.com>
2019-10-31 12:03:42 -0600

    starter server done

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const server = express();
+
+server.listen(4000, () => {
+  console.log('=== Server listening on port 4000 ===');
+});
+
+// console.log('Hello World');
